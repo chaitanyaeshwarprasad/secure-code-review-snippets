85f2f33ce50e19ea0ec5ad9f1323efc70bf58c6d
chaitanyaeshwarprasad <96363656+chaitanyaeshwarprasad@users.noreply.github.com>
2023-07-08 12:49:41 +0530

    Delete securingpassword directory

--- securingpassword/passwordhashfun.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//Secured (Password Hash Function)
-//$password = $_POST['password'];(if it is in the real database)
-$password = "Eshwar@123";
-$hash = password_hash($password, PASSWORD_BCRYPT)
-echo $hash;
-
-?>
--- securingpassword/vulnerable.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//Vulnerable Code
-//$password = $_POST['password'];(if it is in the real database)
-$password = "Eshwar@123"
-echo md5($password)
-
-?>
